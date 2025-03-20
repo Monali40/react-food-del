3ccf2c06b71f89433e84d33ea85d8542a7e8e562
Monali40 <Monalikhot00@gmail.com>
2025-03-20 00:01:36 -0400

    created Navbar

--- food-delivery-website/src/components/Navbar/Navbar.jsx
+++ food-delivery-website/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,27 @@ import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" />
+        <img src={assets.logo} alt="" className='logo'/>
+        <ul className="navbar-menu">
+          <li>home</li>
+          <li>menu</li>
+          <li>mobile-app</li>
+          <li>contact us</li>
+        </ul>
+        <div className="nvarbar-right">
+          <img src={assets.search_icon} alt=""/>
+
+          <div className="navbar-search-icon">
+              <img src={assets.basket_icon} alt=""/>
+
+              <div className="dot"></div>
+          </div>
+              
+              <button>Sing In</button>
+         
+
+        </div>
+
     </div>
   )
 }
